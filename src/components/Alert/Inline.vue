7aefdc1dbc3ff3d2219f8db9f62a5adcb55e7f62
welliton gervickas <wellitogervickas@gmail.com>
2020-09-01 14:59:38 -0300

    background logi, alert component and greating component

---
+++ src/components/Alert/Inline.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="alert__inline">
+    <slot />
+  </div>
+</template>
---
+++ src/components/Greeting/Personal.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="greeting__personal">
+    <strong>{{prefix}}, </strong>
+    <span>{{sufix}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'components-greeting-personal',
+  props: {
+    prefix: {
+      type: String,
+      required: true,
+    },
+    sufix: {
+      type: String,
+      default: 'Bem-vindo',
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.greeting__personal {
+  margin-top: 1rem;
+
+  strong {
+    display: block;
+  }
+}
+</style>
